962557876534c5d4a983db2bd90cf26fb38b651f
Gyozo Nyari <gyozo.nyari@gmail.com>
2015-05-30 22:11:50 +0200

    Cloud config server

---
+++ src/main/java/cloudconfig/Application.java
@@ -0,0 +1,20 @@
+package cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setWebEnvironment(true);
+        app.run(args);
+    }
+}
